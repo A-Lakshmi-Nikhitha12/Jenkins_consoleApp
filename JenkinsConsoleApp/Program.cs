7baa80915c2c83af121fc0f7de576597580fba62
Lakshmi Athukuri <lakshmi_athukuri@epam.com>
2022-08-24 07:55:59 +0530

    updates

---
+++ JenkinsConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace JenkinsConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
